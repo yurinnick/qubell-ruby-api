678b00e76a222cfb8648ad5a483ed16c1e5ddc7d
Nikolay Yurin <nyurin@griddynamics.com>
2014-05-26 09:26:32 +0400

    Optimize handle_api_response
    - handle_api_response gets only one parameter: request response
    - 404 error don't show id of failed resource
    - if unknown http status returned - generate QubellError

--- lib/qubell_api.rb
+++ lib/qubell_api.rb
@@ -23,69 +23,69 @@ class QubellAPI
   # Organization methods
   def organizations
     qubell_request('/organizations')
-    .get do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block)
+    .get do |response|
+      return handle_api_responce(response)
     end
   end
 
   # Application methods
   def get_applications(org_id)
     qubell_request("/organizations/#{org_id}/applications")
-    .get do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block, org_id)
+    .get do |response|
+      return handle_api_responce(response, org_id)
     end
   end
 
   def get_revisions(app_id)
     qubell_request("/applications/#{app_id}/revisions")
-    .get do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block, app_id)
+    .get do |response|
+      return handle_api_responce(response, app_id)
     end
   end
 
   def update_application_manifest(app_id, manifest_path)
     qubell_request("/applications/#{app_id}/manifest")
     .put(File.read(manifest_path), content_type: 'application/x-yaml'
-      ) do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block, app_id)
+      ) do |response|
+      return handle_api_responce(response, app_id)
     end
   end
 
   # Instance methods
   def get_instances(env_id)
     qubell_request("/environments/#{env_id}/instances")
-    .get do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block, env_id)
+    .get do |response|
+      return handle_api_responce(response, env_id)
     end
   end
 
   def get_instance_status(instance_id)
     qubell_request("/revisions/#{rev_id}/instances")
-    .get do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block, instance_id)
-    end    
+    .get do |response|
+      return handle_api_responce(response, instance_id)
+    end
   end
 
   def launch_workflow(instance_id, workflow)
     qubell_request("/instances/#{instance_id}/#{workflow}")
-    .get do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block, instance_id)
-    end  
+    .get do |response|
+      return handle_api_responce(response, instance_id)
+    end
   end
 
   def upload_user_data(instance_id, userdata)
     qubell_request("/instances/#{instance_id}/userData")
-    .put(userdata) do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block, instance_id)
+    .put(userdata) do |response|
+      return handle_api_responce(response, instance_id)
     end
   end
 
   def destroy_instance(instance_id, force)
     force ||= false
     qubell_request("/instances/#{instance_id}?#{force}")
-    .delete do |response, request, result, &block|
-      return handle_api_responce(response, request, result, &block, instance_id)
-    end  
+    .delete do |response|
+      return handle_api_responce(response, instance_id)
+    end
   end
 
   private
@@ -95,15 +95,15 @@ class QubellAPI
     .new("#{@endpoint}/api/#{@api_version}#{path}", @username, @password)
   end
 
-  def handle_api_responce(response, request, result, &block, resource_id = nil)
+  def handle_api_responce(response)
     case response.code
     when 200 then JSON.load response
     when 400 then fail DestroyError, 'instance is either a submodule or active'
     when 401 then fail AuthenticationError, 'invalid credentials'
     when 403 then fail PermissionsError, 'insufficient privileges'
-    when 404 then fail ArgumentError, '#{resource_id} doesn’t exist'
+    when 404 then fail ArgumentError, 'resource doesn’t exist'
     when 409 then fail ArgumentError, 'another workflow is already running'
-    else response.return!(request, result, &block)
+    else fail QubellError, '(#{response.code}) unknown status code'
     end
   end
 end

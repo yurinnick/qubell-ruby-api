2a5a0cd8e94cc777a56e37420995f2c807361c44
Nikolay Yurin <nyurin@griddynamics.com>
2014-06-02 04:03:58 +0400

    Add functions documentation

--- lib/qubell_api.rb
+++ lib/qubell_api.rb
@@ -1,5 +1,6 @@
 # Encoding: utf-8
 # Qubell API wrapper
+# @author Nikolay Yurin <yurinnick@outlook.com>
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
@@ -15,6 +16,12 @@ require 'errors/destroy_error'
 class QubellAPI
   attr_accessor :endpoint, :api_version
 
+  # @param [Hash] params the parameters to configure an API calls with.
+  # @option params [String] :endpoint ('https://express.qubell.com') API endpoint
+  # @option params [String] :api_version ('1') API version
+  # @option params [String] :username
+  # @option params [String] :password
+  # @return [#self]
   def initialize(params = {})
     @endpoint = params.fetch(:endpoint, 'https://express.qubell.com')
     @api_version = params.fetch(:api_version, '1')
@@ -22,7 +29,8 @@ class QubellAPI
     @password = params.fetch(:password)
   end
 
-  # Organization methods
+  # Get list of all organizations that current user belong to.
+  # @return [Array<Hashes>] organizations info
   def organizations
     qubell_request('/organizations')
     .get do |response|
@@ -30,7 +38,9 @@ class QubellAPI
     end
   end
 
-  # Application methods
+  # Get list of all applications in given organization.
+  # @param [String] org_id queried organization id
+  # @return [Array<Hashes>] applications info
   def get_applications(org_id)
     qubell_request("/organizations/#{org_id}/applications")
     .get do |response|
@@ -38,6 +48,9 @@ class QubellAPI
     end
   end
 
+  # Get list of all named revisions for given application.
+  # @param [String] app_id queried application id
+  # @return [Array<Hashes>] revisions info
   def get_revisions(app_id)
     qubell_request("/applications/#{app_id}/revisions")
     .get do |response|
@@ -45,6 +58,9 @@ class QubellAPI
     end
   end
 
+  # Get the application instance status.
+  # @param [String] instance_id queried instance id
+  # @return [Hashes] instances status info
   def update_application_manifest(app_id, manifest_path)
     qubell_request("/applications/#{app_id}/manifest")
     .put(File.read(manifest_path), content_type: 'application/x-yaml'
@@ -53,7 +69,9 @@ class QubellAPI
     end
   end
 
-  # Instance methods
+  # Get list of instances launched in given environment.
+  # @param [String] env_id queried environment id
+  # @return [Array<Hashes>] instances info
   def get_instances(env_id)
     qubell_request("/environments/#{env_id}/instances")
     .get do |response|
@@ -61,6 +79,9 @@ class QubellAPI
     end
   end
 
+  # Get the application instance status.
+  # @param [String] instance_id queried instance id
+  # @return [Hashes] instances status info
   def get_instance_status(instance_id)
     qubell_request("/revisions/#{rev_id}/instances")
     .get do |response|
@@ -68,6 +89,10 @@ class QubellAPI
     end
   end
 
+  # Runs the specified application instance workflow.
+  # @param [String] queried instance_id
+  # @param [String] workflow name
+  # @return [Hashes] instance id
   def launch_workflow(instance_id, workflow)
     qubell_request("/instances/#{instance_id}/#{workflow}")
     .get do |response|
@@ -75,6 +100,10 @@ class QubellAPI
     end
   end
 
+  # Associates user data with the instance.
+  # @param [String] instance_id instance to associate with
+  # @param [String] userdata JSON-encoded user data
+  # @return [#void]
   def upload_user_data(instance_id, userdata)
     qubell_request("/instances/#{instance_id}/userData")
     .put(userdata) do |response|
@@ -82,6 +111,10 @@ class QubellAPI
     end
   end
 
+  # Destroy an application
+  # @param [String] instance_id instance to destroy
+  # @param [String] force specifies whether the instance should be deleted
+  # @return [#void]
   def destroy_instance(instance_id, force)
     force ||= false
     qubell_request("/instances/#{instance_id}?#{force}")
@@ -92,6 +125,9 @@ class QubellAPI
 
   private
 
+  # Create http request with basic auth
+  # @param [String] path API resource path
+  # return [RestClient::Resource]
   def qubell_request(path)
     RestClient::Resource
     .new("#{@endpoint}/api/#{@api_version}#{path}", @username, @password)

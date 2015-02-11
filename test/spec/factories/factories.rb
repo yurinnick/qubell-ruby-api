b771df50b57fa158bfd598621050ec4d407a0394
Nikolay Yurin <nyurin@griddynamics.com>
2015-02-12 01:02:00 +0300

    Improve factories
    - Add dynamic @organization attribute into Qubell::Application class factory
    - Add Qubell::Instance class factory

--- test/spec/factories/factories.rb
+++ test/spec/factories/factories.rb
@@ -4,6 +4,7 @@ require 'qubell/configuration'
 require 'qubell/organization'
 require 'qubell/revision'
 require 'qubell/environment'
+require 'qubell/instance'
 
 FactoryGirl.define do
   factory :client, class: Qubell::API
@@ -18,11 +19,30 @@ FactoryGirl.define do
   end
 
   factory :application, class: Qubell::Application do
-    initialize_with { new(id: SecureRandom.hex(12), name: 'app') }
+    transient do
+      org_id SecureRandom.hex(12)
+    end
+    initialize_with do
+      new(
+          id: SecureRandom.hex(12),
+          name: 'app',
+          organization: org_id
+      )
+    end
   end
 
   factory :revision, class: Qubell::Revision do
     initialize_with { new(id: SecureRandom.hex(12), name: 'rev') }
+  end
+
+  factory :instance, class: Qubell::Instance do
+    initialize_with do
+      new(
+          id: SecureRandom.hex(12),
+          name: 'instance',
+          environment: SecureRandom.hex(12),
+          revision: SecureRandom.hex(12))
+    end
   end
 
   factory :environment, class: Qubell::Environment do

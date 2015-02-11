4f5eff828304e9645afe525530f67688467b1aff
Nikolay Yurin <nyurin@griddynamics.com>
2015-02-12 00:59:59 +0300

    Improve Qubell::Organization
    - Fix Qubell::Application initial map
    - Add #environments method

--- lib/qubell/organization.rb
+++ lib/qubell/organization.rb
@@ -4,6 +4,7 @@
 
 require 'qubell/base'
 require 'qubell/application'
+require 'qubell/environment'
 require 'qubell/api_call'
 
 module Qubell
@@ -11,7 +12,14 @@ module Qubell
   class Organization < Base
     def applications
       Qubell::APICall.get("/organizations/#{@id}/applications").map do |app|
+        app['organization_id'] = @id
         Qubell::Application.new(app)
+      end
+    end
+
+    def environments
+      Qubell::APICall.get("/organizations/#{@id}/environments").map do |app|
+        Qubell::Environment.new(app)
       end
     end
   end

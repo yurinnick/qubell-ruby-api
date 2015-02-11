a34811d7b3545e5d033d7d3e1460341b2cf8ebb2
Nikolay Yurin <yurinnick@outlook.com>
2015-02-12 00:54:25 +0300

    Rework Qubell::Instance class
    - Add @environment and @revision attributes
    - Add #instance_of_app?, #version, #parameters and #status methods
    - Add private method #global_info
    - Fix API call methods

--- lib/qubell/instance.rb
+++ lib/qubell/instance.rb
@@ -1,24 +1,34 @@
+# Encoding: utf-8
+# Qubell application class
+# @author Nikolay Yurin <yurinnick@outlook.com>
+
+require 'qubell/base'
+
 module Qubell
   # Implement Qubell application interface
   class Instance < Base
-    # Get the application instance status.
-    # @return [Hashes{String => String,Hash}] instances status info
-    def status
-      get("/instances/#{@id}")
+    attr_reader :environment
+    attr_reader :revision
+
+    def initialize(args)
+      super
+      @environment = args[:environment]
+      @revision = args[:revision]
     end
 
     # Runs the specified application instance workflow.
     # @param [String] workflow instance workflow name
+    # @param [Hash{String => String}] workflow instance workflow name
     # @return [Hashes{String => String}] instance id
-    def launch_workflow(workflow)
-      get("/instances/#{@id}/#{workflow}")
+    def launch_workflow(workflow, _args)
+      Qubell::APICall.get("/instances/#{@id}/#{workflow}")
     end
 
     # Associates user data with the instance.
     # @param [String] userdata JSON-encoded user data
     # @return [#void]
     def upload_user_data(userdata)
-      put("/instances/#{@id}/userData", userdata)
+      Qubell::APICall.put("/instances/#{@id}/userData", userdata)
     end
 
     # Destroy an application
@@ -26,7 +36,23 @@ module Qubell
     # @return [#void]
     def destroy(force = false)
       status = force ? '1' : '0'
-      delete("/instances/#{@id}?#{status}")
+      Qubell::APICall.delete("/instances/#{@id}?#{status}")
+    end
+
+    def instance_of_app?(app)
+      global_info[:applicationId] == app.id
+    end
+
+    %w(version parameters status).each do |method_name|
+      define_method(method_name.to_sym) do
+        self.global_info[method_name]
+      end
+    end
+
+    private
+
+    def global_info
+      Qubell::APICall.get("/instances/#{@id}")
     end
   end
 end

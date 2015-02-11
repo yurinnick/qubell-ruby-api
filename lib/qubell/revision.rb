9ff5a189ad246d6e42c4788ba4cf4d8fb23a46bd
Nikolay Yurin <nyurin@griddynamics.com>
2015-02-12 00:58:55 +0300

    Add #instances method for Qubell::Revision

--- lib/qubell/revision.rb
+++ lib/qubell/revision.rb
@@ -2,5 +2,10 @@ require 'qubell/base'
 
 module Qubell
   class Revision < Base
+    def instances
+      Qubell::APICall.get("/revisions/#{id}/instances").map do |instance|
+        Qubell::Instance.new(instance)
+      end
+    end
   end
 end

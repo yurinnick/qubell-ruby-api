5f21e8ab2d58af5cb27f10d6adfedc611e7f9515
Nikolay Yurin <yurinnick@outlook.com>
2015-02-13 10:46:56 +0300

    Make @endpoint attribute read-only in Qubell::Configuration class

--- lib/qubell/configuration.rb
+++ lib/qubell/configuration.rb
@@ -14,12 +14,16 @@ module Qubell
 
   # Store configuration settings
   class Configuration
-    attr_accessor :domain, :api_version, :endpoint, :username, :password
+    attr_accessor :domain, :api_version, :username, :password
+    attr_reader :endpoint
 
     def initialize
       @domain = 'https://express.qubell.com'
       @api_version = '1'
-      @endpoint = "#{@domain}/api/#{@api_version}"
+    end
+
+    def endpoint
+      "#{@domain}/api/#{@api_version}"
     end
 
     def to_s

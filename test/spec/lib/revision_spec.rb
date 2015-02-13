9a58e509aecfefded9cfb03790f45029517985a8
Nikolay Yurin <nyurin@griddynamics.com>
2015-02-13 10:45:28 +0300

    Add tests for Qubell::Revision class

---
+++ test/spec/lib/revision_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+
+module Qubell
+  describe Revision do
+    let(:config) { FactoryGirl.build :configuration }
+    let(:revision) { FactoryGirl.build :revision }
+    let(:revision_url) { "#{config.endpoint}/revisions/#{revision.id}" }
+    let(:instances) { FactoryGirl.build_list(:instance, 2) }
+
+    describe '#instances' do
+      before do
+        stub_request(:get, "#{revision_url}/instances")
+        .to_return(
+            status: 200,
+            body: instances.to_json,
+            headers: { :'Content-type' => 'application/json' })
+      end
+      it 'return array of application instances' do
+        expect(revision.instances).to match_array(instances)
+      end
+    end
+  end
+end
